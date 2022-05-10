13cfdbb543315c19a7149d2d8145c6ca0606c458
Anri Digholm <anridig@gmail.com>
2022-05-11 08:22:24 +0900

    feat: Added flags to example.

--- examples/ema_9_21_trading_strategy/main.go
+++ examples/ema_9_21_trading_strategy/main.go
@@ -11,15 +11,19 @@ import (
 	"github.com/anrid/traderbot/pkg/coingecko"
 	"github.com/anrid/traderbot/pkg/jsoncache"
 	"github.com/anrid/traderbot/pkg/trade"
+	"github.com/spf13/pflag"
 )
 
 func main() {
+	id := pflag.String("id", "terra-luna", "CoinGecko ID (default: terra-luna)")
+	initialInvestment := pflag.Float64("invest", 10_000.00, "Initial investment (default: 10,000.00 USD)")
+	startDaysAgo := pflag.UintP("days-ago", "d", 365, "Start our trading strategy 365 days ago counting from today (default: 365)")
+
+	pflag.Parse()
+
 	cg := coingecko.New(coingecko.USD)
 
-	id := "terra-luna"          // CoinGecko ID.
-	var periodInDays uint = 365 // Start our trading strategy 365 days ago counting from today.
-
-	m, err := cg.MarketChartWithCache(id, periodInDays, jsoncache.InvalidateDaily)
+	m, err := cg.MarketChartWithCache(*id, *startDaysAgo, jsoncache.InvalidateDaily)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,7 +36,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	initialInvestment := 10_000.0 // USD.
-
-	trade.ExecuteTradesAndPrint("9-Day/21-Day EMS CrossOver Strategy", initialInvestment, s.Trades)
+	trade.ExecuteTradesAndPrint("9-Day/21-Day EMS CrossOver Strategy", *initialInvestment, s.Trades)
 }
